9c2ad8e04859e0fd56a08287f5cf2a8cbe53da5d
AQtun81 <lysy9990@gmail.com>
2025-11-24 05:07:20 +0100

    release 5.5.0

--- build-prep.ps1
+++ build-prep.ps1
@@ -12,8 +12,9 @@ New-Item -Force -ItemType directory -Path temp | Out-Null
 (Get-Content -Path "lzav_macro.h") | Out-File -FilePath "temp/lzav.h" -Encoding UTF8
 foreach ($line in Get-Content -Path "lzav_src/lzav.h") {
     if ($line -match $pattern) {
-        $line = $line -replace '\bLZAV_INLINE_F\b', 'LZAV_EXPORT'
-        $line = $line -replace '\bLZAV_INLINE\b',   'LZAV_EXPORT'
+        $line = $line -replace '\bLZAV_INLINE_F\b',  'LZAV_EXPORT'
+        $line = $line -replace '\bLZAV_INLINE\b',    'LZAV_EXPORT'
+        $line = $line -replace '\bLZAV_NO_INLINE\b', 'LZAV_EXPORT'
     }
     $line | Out-File -FilePath "temp/lzav.h" -Encoding UTF8 -Append
 }
